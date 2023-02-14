a703455d3597b8c5ff024c2a3776974dd209a99e
Reajul Hasan Raju <101093517+ujaRHR@users.noreply.github.com>
2023-02-14 12:11:02 +0600

    Desktop Drinking Reminder

---
+++ Desktop-Notifier/main.py
@@ -0,0 +1,18 @@
+import datetime, time, os
+from plyer import notification
+
+state = None
+try:
+    state = "Script is working fine..."
+except:
+    print("Please! Check your internet connection")
+
+if (state != None):
+    while(True):
+        notification.notify(
+            title = "Drinking Water Reminder",
+            message = "You are not a Cactus! Drink some Water. \rIt's important to stay hydrated.",
+            app_icon = f"{os.getcwd()}/icon.ico",
+            timeout  = 5
+        )
+        time.sleep(20) # Reminder after 45 minutes
